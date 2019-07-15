2c0ac20ca23142113335c2c3957d32bc49697a9d
twisteroidambassador <twisteroidambassador@users.noreply.github.com>
2019-07-16 01:48:00 +0800

    Update copyright header.

--- detour_proxy.py
+++ detour_proxy.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-# Copyright 2017, 2018 twisteroid ambassador
+# Copyright 2017 - 2019 twisteroid ambassador
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
